17e73918f2f852ceb56a62db4ef8e22f80d85780
Awais Malik <165843085+19497awaismalik@users.noreply.github.com>
2024-10-07 00:10:38 +0500

    Update Login.tsx

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -44,7 +44,7 @@ const Login: FC<Props> = ({ setroute, setOpen }) => {
 
         }
         if (error) {
-            toast.error((error as any)?.data.messagse);
+            toast.error((error as any)?.data.message);
 
         }
 
